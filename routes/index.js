594f07287ec308bdf6403b2d669647e04999cb93
alexandresuire <alexandresuire75002@gmail.com>
2023-04-19 11:38:37 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+const apiKey = process.env.OWM_API_KEY;
+
+router.get('/movies', (req, res) => {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`)
+.then(response => response.json())
+.then(data => {
+res.json({ movies: data.results });
+});
+});
+
+
+
+
+module.exports = router;
+
+
+
+
